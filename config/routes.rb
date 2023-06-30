15539baf5375470644291d199bde5ceaafe9f19d
AckimJnr <acklongwe@gmail.com>
2023-06-30 08:26:22 +0200

    Bootstrapify Login page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
+  devise_for :users
+
+  devise_scope :user do
+    get "/users/sign_out", to: "devise/sessions#destroy"
+  end
+
+
+
   resources :friends
   root "home#index"
   get "/home/about/", to: "home#about"
